bcbdc757303b1a7e5b2baac4154303de2d919d8e
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-03-05 23:21:12 +0100

    RELEASE-1.1.0

--- eratosthenes.py
+++ eratosthenes.py
@@ -2,26 +2,47 @@
 import time
 
 
-def crible_era(n):
-    start = time.time()
-    firsts = {1, 2}
-    actual_first = 3
-    list_checker = [False] * n
-    while actual_first < n:
-        if not list_checker[actual_first]:
-            firsts.add(actual_first)
-            deleting = actual_first
-            while deleting < n:
-                list_checker[deleting] = True
-                deleting += actual_first
-        actual_first += 2
-    end = time.time() - start
-    print("\nTime of execution : " + str(end) + " secondes.")
-    return firsts
+# Define main method
+def crible_era(up_to):
+    time_to_start_exec = time.time()  # Optional
+
+    prime_numbers = {2}  # List of prime numbers
+
+    actual_number = 3  # We start with 3, then continue in the odd numbers. This number is the current number tested.
+
+    list_checker = [False] * up_to  # This list allows us to check if the number to be eliminated or not.
+    # (Eliminate = No prime number)
+    # (Eliminate = True)
+
+    # this loop allows us to test all the numbers starting from x (continuing x + 2 at each turn) as long as
+    # It's less than or equal to the defined maximum number.
+    while actual_number <= up_to:
+        if not list_checker[actual_number]:  # this checks if the number isn't eliminated
+            prime_numbers.add(actual_number)  # adding to the list of prime numbers.
+
+            deletion_of_multiple = actual_number  # create a temporary variable,
+            # which will be used to delete all the multiples of the number we trained in the if loop.
+
+            # this loop allows us to delete all multiples of the current number,
+            # until It's less than or equal to the maximum number defined.
+            while deletion_of_multiple <= up_to:
+                list_checker[deletion_of_multiple] = True  # It eliminates the multiples in question,
+                # by setting them to True in the checklist.
+
+                deletion_of_multiple *= actual_number  # It adds the current number of deletion
+                # by the current number of test. (Continue the table)
+        actual_number += 2
+    time_to_end_exec = time.time() - time_to_start_exec  # Optional
+    print("\nExecution time : " + str(time_to_end_exec) + " seconds.")  # Optional
+    return prime_numbers  # Return the list of prime numbers.
 
 
-x = int(input("x= "))
-Q = crible_era(x + 1)
-print("\bFirst numbers (to " + str(x) + ") :")
-for i in Q:
-    print("-> " + str(i))
+# Exec
+choose_number = int(input("Choose a number to define how far to display prime numbers.\n-> Value = "))
+prime_number = crible_era(choose_number)
+print("\nPrime Numbers (up to " + str(choose_number) + ") : (" + str(len(prime_number)) + " display)")
+print("Tips: The numbers aren't arranged in order.")
+
+# This for a loop allows us to dissect each of the numbers present in the list.
+for number in prime_number:
+    print("-> " + str(number))
